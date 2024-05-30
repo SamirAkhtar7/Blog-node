b388449b309d13ef8375f6321c2e518662e9ea9b
SamirAkhat7 <samir.akhtar444@gmail.com>
2024-05-31 00:12:16 +0530

    main

--- controller/blogcontroller.js
+++ controller/blogcontroller.js
@@ -1,12 +1,12 @@
 exports.bloghome = (req, res) => {
-    res.render('home');
+    res.render('home',{pagetitle:'My Blog'});
 }
 
 exports.blogabout = (req, res) => {
-    res.render('about');
+    res.render('about',{pagetitle:'About'});
 
 }
 
 exports.blogcontact = (req, res) => {
-    res.render('contact');
+    res.render('contact', { pagetitle: 'Contact'});
 }
