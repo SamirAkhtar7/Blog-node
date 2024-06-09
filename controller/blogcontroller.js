ebfa46d2caff505ede0658eafa4db5f185aad1a1
SamirAkhat7 <samir.akhtar444@gmail.com>
2024-06-09 11:56:41 +0530

    main

--- controller/blogcontroller.js
+++ controller/blogcontroller.js
@@ -1,12 +1,32 @@
 let allposts = [];
 const lodash = require("lodash");
+const axios = require('axios');
 const fs = require('fs');
+let blogdata = "";
 exports.bloghome = (req, res) => {
+    if (fs.existsSync('blogs.txt')) {
+        blogdata = fs.readFileSync("blogs.txt", "utf-8");
+         const blogobject = JSON.parse(blogdata);
+         allposts.push(...blogobject);
+    }
     res.render('home',{pagetitle:'My Blog', allposts});
 }
 
 exports.blogabout = (req, res) => {
-    res.render('about',{pagetitle:'About'});
+ 
+    axios.get("https://type.fit/api/quotes").then(resp => {
+           const len = resp.data.lenght;
+           const value = Math.trunc(Math.random() * len );
+        quote = resp.data[value].text;
+        // console.log(resp.data);
+        res.render("about", { pagetitle: "About", quote });
+        
+    }).catch(error => {
+        console.log("connot get api data ",error);
+    })
+
+
+    
 
 }
 
@@ -42,10 +62,10 @@ exports.blogpost = (req, res) => {
     console.log(req.query);
     const { q } = req.query;
     let f = 0;
-
     for (let post of allposts) {
         if (lodash.lowerCase(q) === lodash.lowerCase(post.title)) {
             f = 1;
+            res.render('post', { pagetitle:'blog',post})
             break;
         }
         if (!f) {
